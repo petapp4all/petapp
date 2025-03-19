47b58dcaf51834312f31642a0fc7fb78c290bfcb
adeyemi <mathewadeyemi7654@gmail.com>
2025-03-19 11:19:56 +0100

    testing

--- petrol-auth/server.js
+++ petrol-auth/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./routes/userRoutes.js";
-import connectDB from "./connection.js";
 dotenv.config();
 
 const app = express();
@@ -11,8 +10,6 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-connectDB();
 
 app.use("/api/users", userRouter);
 
